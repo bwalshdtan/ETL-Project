202c84d828dc22290f8520cf73508378126ae6f7
Brian M Walsh <bwalsh@Brian-Walshs-Computer-2.local>
2019-11-18 19:34:30 -0800

    remove old csv files and create new tables

--- create_census_tables.sql
+++ create_census_tables.sql
@@ -1,43 +1,82 @@
-CREATE TABLE 2010Census_age_gender (
-    Geography VARCHAR NOT NULL,
-    Total Population VARCHAR NOT NULL,
-    Male VARCHAR NOT NULL,
-    Female VARCHAR NOT NULL,
-    Median Age (years) VARCHAR NOT NULL,
-    Male Median Age (years) VARCHAR NOT NULL,
-    Female Median Age (years) VARCHAR NOT NULL,
-    Average Household Size VARCHAR NOT NULL,
-    Average Family Size VARCHAR NOT NULL,
-    Persons Under 5 years VARCHAR NOT NULL,
-    Persons Under 18 years VARCHAR NOT NULL,
-    Persons Age 21+ VARCHAR NOT NULL,
-    Persons Age 55+ VARCHAR NOT NULL,
-    Persons Age 60+ VARCHAR NOT NULL,
-    Persons Age 65+ VARCHAR NOT NULL,
-    Percent Population Female VARCHAR NOT NULL,
-    Percent Population Less than 18 VARCHAR NOT NULL
+CREATE TABLE age (
+    age_id VARCHAR NOT NULL PRIMARY KEY,
+    under_5 INTEGER NOT NULL,
+    under_18 INTEGER NOT NULL,
+    21_and_over INTEGER NOT NULL,
+    55_and_over INTEGER NOT NULL,
+    60_and_over INTEGER NOT NULL,
+    65_and_over INTEGER NOT NULL
 );
 
-CREATE TABLE 2010Census_demographics (
-    Geography VARCHAR NOT NULL,
-    Total Population VARCHAR NOT NULL,
-    Total Population of One Race_Asian_Native Hawaiian and other Pacific Islander_NHOPI VARCHAR NOT NULL,
-    Total Population of Two or More Races VARCHAR NOT NULL,
-    White VARCHAR NOT NULL,
-    Black or African American VARCHAR NOT NULL,
-    American Indian and Alaska Native VARCHAR NOT NULL,
-    Total Asian VARCHAR NOT NULL,
-    Asian Indian VARCHAR NOT NULL,
-    Chinese VARCHAR NOT NULL,
-    Filipino VARCHAR NOT NULL,
-    Japanese VARCHAR NOT NULL,
-    Korean VARCHAR NOT NULL,
-    Vietmanese VARCHAR NOT NULL,
-    Other Asian VARCHAR NOT NULL,
-    Total NHOPI VARCHAR NOT NULL,
-    Native Hawaiian VARCHAR NOT NULL,
-    Guamian or Chamooro VARCHAR NOT NULL,
-    Samoan VARCHAR NOT NULL,
-    Pacific Islander VARCHAR NOT NULL,
-    Some Other Race VARCHAR NOT NULL
+CREATE TABLE race (
+    race_id VARCHAR NOT NULL PRIMARY KEY,
+    white VARCHAR NOT NULL,
+    black VARCHAR NOT NULL,
+    native_american VARCHAR NOT NULL,
+    indian VARCHAR NOT NULL,
+    chinese VARCHAR NOT NULL,
+    filipino VARCHAR NOT NULL,
+    japanese VARCHAR NOT NULL,
+    korean VARCHAR NOT NULL,
+    vietmanese VARCHAR NOT NULL,
+    other_Asian VARCHAR NOT NULL,
+    hawaiian VARCHAR NOT NULL,
+    guamian  VARCHAR NOT NULL,
+    samoan VARCHAR NOT NULL,
+    pacific_islander VARCHAR NOT NULL,
+    other VARCHAR NOT NULL
+);
+
+CREATE TABLE sex (
+    sex_id VARCHAR NOT NULL PRIMARY KEY,
+    male VARCHAR,
+    female VARCHAR
+);
+
+CREATE TABLE age_zip (
+    FOREIGN KEY (age_id) REFERENCES age(age_id),
+    FOREIGN KEY (zip_id) REFERENCES zip(zip_id),
+    qty_pct VARCHAR NOT NULL,
+);
+
+CREATE TABLE race_zip (
+    FOREIGN KEY (race_id) REFERENCES race(race_id),
+    FOREIGN KEY (zip_id) REFERENCES zip(zip_id),
+    qty_pct VARCHAR NOT NULL,
+);
+
+CREATE TABLE sex_zip (
+    FOREIGN KEY (sex_id) REFERENCES sex(sex_id),
+    FOREIGN KEY (zip_id) REFERENCES zip(zip_id),
+    qty_pct VARCHAR NOT NULL
+);
+
+CREATE TABLE zip (
+    zip_id VARCHAR NOT NULL PRIMARY KEY,
+    zip INTEGER NOT NULL
+);
+
+CREATE TABLE store_sales (
+    FOREIGN KEY (store_id) REFERENCES store(store_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
+    units INTEGER NOT NULL,
+    zip_id VARCHAR NOT NULL FOREIGN KEY
+);
+
+CREATE TABLE product (
+    product_id VARCHAR NOT NULL PRIMARY KEY,
+    product_number INTEGER NOT NULL,
+    description VARCHAR NOT NULL
+    pack INTEGER NOT NULL,
+    size INTEGER NOT NULL,
+    price FLOAT NOT NULL
+);
+
+CREATE TABLE store (
+    store_id VARCHAR NOT NULL PRIMARY KEY,
+    store_name VARCHAR NOT NULL,
+    address VARCHAR NOT NULL,
+    city VARCHAR NOT NULL,
+    state VARCHAR NOT NULL,
+    zip VARCHAR NOT NULL
 );
