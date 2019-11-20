3fa631c97e18f142126f4dd05558889c43cb9bb6
Brian M Walsh <bwalsh@Brian-Walshs-Computer-2.local>
2019-11-19 21:20:46 -0800

    Add sex table and upate sql tables

--- create_tables.sql
+++ create_tables.sql
@@ -1,41 +1,16 @@
 CREATE TABLE age (
     age_id VARCHAR NOT NULL PRIMARY KEY,
-    under_5 INTEGER NOT NULL,
-    under_18 INTEGER NOT NULL,
-    over_21 INTEGER NOT NULL,
-    over_55 INTEGER NOT NULL,
-    over_60 INTEGER NOT NULL,
-    over_65 INTEGER NOT NULL
+    age_group INTEGER NOT NULL
 );
 
 CREATE TABLE race (
     race_id VARCHAR NOT NULL PRIMARY KEY,
-    white VARCHAR NOT NULL,
-    black VARCHAR NOT NULL,
-    native_american VARCHAR NOT NULL,
-    indian VARCHAR NOT NULL,
-    chinese VARCHAR NOT NULL,
-    filipino VARCHAR NOT NULL,
-    japanese VARCHAR NOT NULL,
-    korean VARCHAR NOT NULL,
-    vietmanese VARCHAR NOT NULL,
-    other_Asian VARCHAR NOT NULL,
-    hawaiian VARCHAR NOT NULL,
-    guamian  VARCHAR NOT NULL,
-    samoan VARCHAR NOT NULL,
-    pacific_islander VARCHAR NOT NULL,
-    other VARCHAR NOT NULL
+    race_group VARCHAR NOT NULL
 );
 
 CREATE TABLE sex (
     sex_id VARCHAR NOT NULL PRIMARY KEY,
-    male VARCHAR,
-    female VARCHAR
-);
-
-CREATE TABLE zip (
-    zip_id VARCHAR NOT NULL PRIMARY KEY,
-    zip INTEGER NOT NULL
+    sex_group VARCHAR
 );
 
 CREATE TABLE store (
@@ -52,25 +27,25 @@ CREATE TABLE product (
     product_number INTEGER NOT NULL,
     description VARCHAR NOT NULL,
     pack_qty INTEGER NOT NULL,
-    size INTEGER NOT NULL,
+    size VARCHAR NOT NULL,
     price FLOAT NOT NULL
 );
 
 CREATE TABLE age_zip (
 	age_id VARCHAR REFERENCES age(age_id),
-    zip_id VARCHAR REFERENCES zip(zip_id),
+    zip VARCHAR NOT NULL,
     qty_pct VARCHAR NOT NULL
 );
 
 CREATE TABLE race_zip (
     race_id VARCHAR REFERENCES race(race_id),
-	zip_id VARCHAR REFERENCES zip(zip_id),
+	zip VARCHAR NOT NULL,
     qty_pct VARCHAR NOT NULL
 );
 
 CREATE TABLE sex_zip (
 	sex_id VARCHAR REFERENCES sex(sex_id),
-	zip_id VARCHAR REFERENCES zip(zip_id),
+	zip VARCHAR NOT NULL,
     qty_pct VARCHAR NOT NULL
 );
 
@@ -78,6 +53,6 @@ CREATE TABLE store_sales (
 	store_id VARCHAR REFERENCES store(store_id),
 	product_id VARCHAR REFERENCES product(product_id),
     units FLOAT NOT NULL,
-	zip_id VARCHAR REFERENCES zip(zip_id)
+	zip VARCHAR NOT NULL
 );
 
